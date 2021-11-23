4a6a46c18e06484c1fda21baccd19ac2a9d2d0d4
sergio-miranda-usa <77934395+sergio-miranda-usa@users.noreply.github.com>
2021-11-22 21:25:50 -0500

    Add project files.

---
+++ Extractor/Crypto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Extractor
+{
+    class Crypto
+    {
+        private static string key = "b14ca5898a4e8675309e2ea2143a2410";
+        public static string EncryptString(string plainText)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+            return Convert.ToBase64String(array);
+        }
+        public static string DecryptString(string cipherText)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);//I have already defined "Key" in the above EncryptString function
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Extractor/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace Extractor
+{
+    class Program
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            Logger.Info("Starting Program...");
+            
+            //Use EncryptedConnectionString for an encrypted string.
+            //Use ConnectionString for cleartext string
+            string connString = GetConfig("ConnectionString");
+            string cryptoConnString = GetConfig("EncryptedConnectionString");
+
+            //Check if encrypted string is used.  If cleartext is used console print encrypted string.
+            if (connString != "") Console.WriteLine(Crypto.EncryptString(connString));
+            if (cryptoConnString != "") connString = Crypto.DecryptString(cryptoConnString);
+            
+            //console print the data
+            GetData(connString);
+            Logger.Info("...End Program");
+        }
+        static string GetConfig(string keyStr)
+        {
+            string retval = "";
+            try
+            {
+                IConfiguration config = new ConfigurationBuilder()
+              .AddJsonFile("AppSettings.json", true, true)
+              .Build();
+
+                if (config[keyStr] != null)
+                    retval = config[keyStr].ToString();
+                
+                return retval;
+            }
+            catch(Exception ex)
+            {
+                Logger.Error(ex, "GetConfig");
+                return retval;
+            }
+
+        }
+        static void GetData(string connectStr)
+        {
+            try
+            {
+                using SqlConnection sql = new SqlConnection(connectStr);
+                using SqlCommand cmd = new SqlCommand("spGetProviderAll", sql);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                sql.Open();
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.Write(DataToString(reader) + "\n");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "GetData");
+            }
+        }
+        private static string DataToString(SqlDataReader reader)
+        {
+            return reader["Provider_ID"].ToString() + "," +
+                reader["Provider"].ToString() + "," +
+                reader["Practice"].ToString() + "," +
+                reader["Specialty_ID"].ToString() + "," +
+                reader["Specialty_Name"].ToString() + "," +
+                reader["Address_1"].ToString() + "," +
+                reader["Address_2"].ToString() + "," +
+                reader["City"].ToString() + "," +
+                reader["State"].ToString() + "," +
+                reader["Postal_Code"].ToString() + "," +
+                reader["Attention"].ToString() + "," +
+                reader["Notes"].ToString() + "," +
+                reader["Inactive"].ToString();
+
+        }
+    }
+}
